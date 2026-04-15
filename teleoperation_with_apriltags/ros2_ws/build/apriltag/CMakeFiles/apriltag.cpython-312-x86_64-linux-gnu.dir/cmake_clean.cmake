c6d782ef4d7b7c579af1b014518cba49c9c817d0
np-223996 <>
2026-04-15 14:56:06 +0200

    sensagram_twist

---
+++ teleoperation_with_apriltags/ros2_ws/build/apriltag/CMakeFiles/apriltag.cpython-312-x86_64-linux-gnu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/apriltag.cpython-312-x86_64-linux-gnu.dir/apriltag_pywrap.c.o"
+  "CMakeFiles/apriltag.cpython-312-x86_64-linux-gnu.dir/apriltag_pywrap.c.o.d"
+  "apriltag.cpython-312-x86_64-linux-gnu.pdb"
+  "apriltag.cpython-312-x86_64-linux-gnu.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/apriltag.cpython-312-x86_64-linux-gnu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
