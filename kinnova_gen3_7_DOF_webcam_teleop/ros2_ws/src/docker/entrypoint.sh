f63a5a1a0f1e1c33549cddae982526c7f803ec99
np-223996 <>
2026-04-16 14:16:38 +0200

    zenoh_bridge

--- kinnova_gen3_7_DOF_webcam_teleop/ros2_ws/src/docker/entrypoint.sh
+++ kinnova_gen3_7_DOF_webcam_teleop/ros2_ws/src/docker/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
+
 source /opt/ros/${ROS_DISTRO}/setup.bash
+
 if [ -f "/ros2_ws/install/setup.bash" ]; then
-    source /ros2_ws/install/setup.bash
+  source /ros2_ws/install/setup.bash
 fi
+
+# Zenoh router config for rmw_zenoh_cpp
+export ZENOH_RUNTIME_CONFIG=/zenoh_config.json5
+
 exec "$@"
